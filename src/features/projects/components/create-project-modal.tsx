7be46db90fa5b2231c9d332e0a397953d2ad1af1
Earl Ericson Endricoso <projogoalamb2017@gmail.com>
2025-01-07 13:02:45 +0800

    jira-clone updated_6

---
+++ src/features/projects/components/create-project-modal.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ResponsiveModal } from "@/components/responsive-modal";
+
+import { CreateProjectForm } from "./create-project-form";
+import { UseCreateProjectModal } from "../hooks/use-create-project-modal";
+
+export const CreateProjectModal = () => {
+    const { isOpen, setIsOpen, close } = UseCreateProjectModal();
+
+    return (
+        <ResponsiveModal open={isOpen} onOpenChange={() => { }}>
+            <CreateProjectForm onCancel={close} />
+        </ResponsiveModal>
+    );
+};
