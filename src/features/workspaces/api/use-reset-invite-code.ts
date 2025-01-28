1d6a301e6bc238c779260cee82c732bff9f00663
Earl Ericson Endricoso <projogoalamb2017@gmail.com>
2025-01-28 12:03:49 +0800

    jira-clone updated_18

--- src/features/workspaces/api/use-reset-invite-code.ts
+++ src/features/workspaces/api/use-reset-invite-code.ts
@@ -3,13 +3,11 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
-import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaceId"]["reset-invite-code"]["$post"], 200>;
 type RequestType = InferRequestType<typeof client.api.workspaces[":workspaceId"]["reset-invite-code"]["$post"]>;
 
 export const useResetInviteCode = () => {
-    const router = useRouter();
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -29,7 +27,6 @@ export const useResetInviteCode = () => {
         onSuccess: ({ data }) => {
             toast.success("Invite code reset");
 
-            router.refresh();
             queryClient.invalidateQueries({ queryKey: ["workspaces"] });
             queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] });
         },
