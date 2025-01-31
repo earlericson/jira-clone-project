d6be58d3d76e265b03029e0d8922ff6e449d7b97
Earl Ericson Endricoso <projogoalamb2017@gmail.com>
2025-01-31 10:17:48 +0800

    jira-clone updated_21.2

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,13 +1,14 @@
 import {toast} from "sonner";
+import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
-
 import { client } from "@/lib/rpc";
 
 type ResponseType = InferResponseType<typeof client.api.workspaces["$post"]>;
 type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>;
 
 export const useCreateWorkspace = () => {
+    const router = useRouter();
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -26,6 +27,8 @@ export const useCreateWorkspace = () => {
         },
         onSuccess: () => {
             toast.success("Workspace created");
+
+            router.refresh();
             queryClient.invalidateQueries({ queryKey: ["workspaces"] });
         },
         onError: () => {
